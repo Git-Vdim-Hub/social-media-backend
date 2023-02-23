cd6740945371c75c16b10aa87b6531379a469f00
Git-Vdim-Hub <vadimrace@gmail.com>
2023-02-23 17:25:55 -0500

    dependencies installed, more framework added, server running on port 3001

---
+++ config/connection.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb://127.0.0.1:27017/socialMedia', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = mongoose.connection;
