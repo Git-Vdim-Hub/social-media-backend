6a1af91f508cb6c4a766f61601c1a29c51ddc5aa
Git-Vdim-Hub <vadimrace@gmail.com>
2023-02-24 14:39:48 -0500

    get all users, post one user

--- models/User.js
+++ models/User.js
@@ -3,10 +3,41 @@ const { Schema, model} = require ('mongoose');
 // Creating the User model with the mongoose Schema
 const userSchema = new Schema(
     {
-        username: {type: String,
-                   unique: true,
-                   required: true,
-                   
-                   }
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please provide a valid email address']
+
+        },
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }]
     },
-)
+    {
+        toJSON:{
+            virtuals: true,
+        },
+        id: false
+    }
+);
+console.log("PIKACHU@35");
+userSchema.virtual('friendCount').get(function(){
+    return this.friends.length;
+})
+
+const User = model('User', userSchema);
+//User.create([{username: 'test', email: 'test123@email.com'}])
+
+module.exports = User
---
+++ utils/seed.js
@@ -0,0 +1,8 @@
+// const connection = require('../config/connection');
+// const {getRandomName} = require('./data');
+
+// console.time('seeding');
+
+// connection.once('open', async () => {
+
+// })
