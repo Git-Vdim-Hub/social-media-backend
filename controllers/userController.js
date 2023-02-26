ea47f080fc2bbd49cad85ee6fc4b95d5c3c771ac
Git-Vdim-Hub <vadimrace@gmail.com>
2023-02-25 21:41:44 -0500

    deleteFriend created

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,8 @@ module.exports = {
   // Get all users
   getUsers(req, res) {
     User.find()
-      .populate('thoughts')//{ path: 'thoughts', select: '-__v' })
+      .populate('thoughts')
+      .populate('friends')//{ path: 'thoughts', select: '-__v' })
       .then((users) => res.json(users))
       .catch((err) => res.status(500).json(err));
   },
@@ -14,6 +15,7 @@ module.exports = {
     User.findOne({ _id: req.params.userId })
       .select('-__v')
       .populate('thoughts')
+      .populate('friends')
       .then((user) =>
         !user
           ? res.status(404).json({ message: 'No user with that ID' })
@@ -27,9 +29,21 @@ module.exports = {
       .then((user) => res.json(user))
       .catch((err) => res.status(500).json(err));
   },
-  
+  // update a user
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      {_id: req.params.userId},
+      {$set: req.body},
+      {runValidators: true, new: true}
+    )
+    .then((user) =>
+    !user
+    ? res.status(404).json({message: 'No user with this id!'})
+    : res.json(user)
+    )
+    .catch((err) => res.status(500).json(err));
+  },
   // Delete a user and associated thoughts
-
   deleteUser(req, res) {
     User.findOneAndDelete({ _id: req.params.userId })
       .then((user) =>
@@ -40,4 +54,42 @@ module.exports = {
       .then(() => res.json({ message: 'User and associated thoughts deleted!' }))
       .catch((err) => res.status(500).json(err));
   },
+   //add a friend to the user
+  addFriend(req, res) {
+    console.log('You are adding a friend');
+    //console.log(req);
+    User.findOneAndUpdate(
+      {_id: req.params.userId},
+      {$addToSet: {friends: req.params.friendId}},
+      {runValidators: true, new: true}
+    )
+      .then((user) =>
+      !user
+        ? res
+          .status(404)
+          .json({message: 'No user found with this ID'})
+          : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));
+  },
+  //delete a friend from a user
+  deleteFriend(req, res){
+    console.log('You are removing a friend');
+    User.findOneAndUpdate(
+      {_id: req.params.userId},
+      {$pull: {friends: req.params.friendId}},
+      {runValidators: true, new: true}
+    )
+    .then((user) =>
+    !user
+    ? res.status(404).json({
+      message: 'No user found with this ID'
+    })
+    : res.json({message: 'Friend successfully removed'})
+    )
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    })
+  }
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,13 +3,19 @@ const {
     getUsers,
     getSingleUser,
     createUser,
-    deleteUser
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
   } = require('../../controllers/userController');
   
   //http://localhost:3001/api/users
   router.route('/').get(getUsers).post(createUser);
   
   //http://localhost:3001/api/users/:userId
-  router.route('/:userId').get(getSingleUser).delete(deleteUser);
+  router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+ //http://localhost:3001/api/users/:userId/friends/:friendId
+  router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
+
   
   module.exports = router;
